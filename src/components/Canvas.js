c8841d205938927184aa0c03836e15cb4fee138a
roosap <roosa.paivansalo@gmail.com>
2020-05-12 21:18:13 +0100

    Fix cursor pointing

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,57 +1,39 @@
 import React, {useRef, useState} from 'react';
-
-function draw(ctx, posX, posY) {
-    ctx.beginPath();
-    ctx.moveTo(posX - 5, posY);
-    ctx.lineTo(posX, posY - 7);
-    ctx.lineTo(posX + 5, posY);
-    ctx.arc(posX, posY, 5, 0, Math.PI);
-    ctx.closePath();
-    ctx.fillStyle = 'white';
-    ctx.fill();
-
-    // const words = ["you look nice", "hello", "have a great day", "you're the best", "hey"]
-    // ctx.font = "1.2em Helvetica";
-    // ctx.fillStyle = "white";
-    // ctx.textAlign = "center";
-    // ctx.fillText(words[Math.floor(Math.random() * words.length)], posX, posY);
-}
+import {Link} from 'react-router-dom';
 
 const Canvas = () => {
     const canvasRef = useRef(null);
+    const divRef = useRef(null);
     const [isDrawing, setIsDrawing] = useState(false);
     const [x, setX] = useState(0);
     const [y, setY] = useState(0);
 
-    // const handleCanvasClick = (e) => {
-    //     const canvas = canvasRef.current
-    //     const ctx = canvas.getContext('2d')
-    //     draw(ctx, e.clientX, e.clientY)
-    // }
-
     const handleMouseDown = (e) => {
-        setX(e.clientX);
-        setY(e.clientY);
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        const rect = canvas.getBoundingClientRect();
+        setX(e.clientX - rect.left);
+        setY(e.clientY - rect.top);
         setIsDrawing(true);
-        console.log("mousedown X", x, "mousedownY:", y)
     }
 
     const handleMouseMove = (e) => {
-        const canvas = canvasRef.current
-        const ctx = canvas.getContext('2d')
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        const rect = canvas.getBoundingClientRect();
         if (isDrawing === true) {
-            drawLine(ctx, x, y, e.clientX, e.clientY);
-            setX(e.clientX);
-            setY(e.clientY);
-            console.log("mousemove X", x, "mousedownY:", y)
+            drawLine(ctx, x, y, e.clientX - rect.left, e.clientY - rect.top);
+            setX(e.clientX - rect.left);
+            setY(e.clientY - rect.top);
         }
     }
 
     const handleMouseUp = (e) => {
-        const canvas = canvasRef.current
-        const ctx = canvas.getContext('2d')
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        const rect = canvas.getBoundingClientRect();
         if (isDrawing === true) {
-            drawLine(ctx, x, y, e.clientX, e.clientY);
+            drawLine(ctx, x, y, e.clientX - rect.left, e.clientY - rect.top);
             setX(0);
             setY(0);
             setIsDrawing(false);
@@ -61,16 +43,19 @@ const Canvas = () => {
     const drawLine = (context, x1, y1, x2, y2) => {
         context.beginPath();
         context.strokeStyle = 'white';
-        context.lineWidth = 5;
+        context.lineWidth = 10;
         context.moveTo(x1, y1);
         context.lineTo(x2, y2);
         context.stroke();
         context.closePath();
     }
 
-
     return ( 
-        <div>
+        <div ref={divRef}>
+            <h4>Nice to meet you!</h4>
+            <h6>If you're here to read more about me, head to my <span className="white"><Link to='/about'>About Me</Link></span> section.<br /><br />
+            If you're here to have a sneak peek at my recent projects, please see <span className="white"><Link to='/projects'>My Projects</Link></span>.<br /><br />
+            Or, if you're here because you're bored, why don't you draw something below:</h6>
                 <canvas
                     ref={canvasRef}
                     width={window.innerWidth}
@@ -78,6 +63,7 @@ const Canvas = () => {
                     onMouseDown={handleMouseDown}
                     onMouseMove={handleMouseMove}
                     onMouseUp={handleMouseUp}>
+                        <h6>Sorry, your browser doesn't support this element!</h6>
                 </canvas>
         </div>
      );
