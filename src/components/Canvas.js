ce15dbec5c2ab88a44fed90c422abaf23f7e9110
roosap <roosa.paivansalo@gmail.com>
2020-05-12 16:35:05 +0100

    Play with raindrops on canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,67 +1,35 @@
-import React, {Component} from 'react';
-import ProfilePicture from '../assets/images/roosapp_circle.png';
+import React, {useRef} from 'react';
 
-class Canvas extends Component {
-
-    componentDidMount() {
-        const canvas = this.refs.canvas
-        const ctx = canvas.getContext("2d")
-        const start = null
-        const drops = [
-            { x: 180, y: 0 },
-            { x: 280, y: 50 },
-            { x: 350, y: 120 },
-            { x: 470, y: 40 },
-            { x: 300, y: 170 },
-            { x: 400, y: 290 },
-            { x: 500, y: 0 },
-            { x: 160, y: 220 }
-        ]
-
-      const requestAnimationFrame = (anim) => {
-    }
-
-      const rain = (posX, posY) => {
-            ctx.beginPath();
-            ctx.moveTo(posX - 5, posY);
-            ctx.lineTo(posX, posY - 7);
-            ctx.lineTo(posX + 5, posY);
-            ctx.arc(posX, posY, 5, 0, Math.PI);
-            ctx.closePath();
-            ctx.fillStyle = '#7094cf';
-            ctx.fill();
-      }
-
-      const anim = (timestamp) => {
-        if (!start) start = timestamp;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        draw()
-        if ((timestamp - start) < 2000) {
-            start = null;
-            requestAnimationFrame(anim)
-          }
-      }
-
-      const draw = () => {
-        drops.forEach((drop) => {
-            rain(drop.x, drop.y);
-            drop.y += 1;
-            if (drop.y > canvas.height) {
-            drop.y = 0;
-            }
-        }
-        )}
-    }
-
-
-    render() { 
-        return ( 
-            <div>
-                <canvas ref="canvas" width={640} height={425}></canvas>
-            </div>
-         );
-    }
+function draw(ctx, posX, posY) {
+    ctx.beginPath();
+    ctx.moveTo(posX - 5, posY);
+    ctx.lineTo(posX, posY - 7);
+    ctx.lineTo(posX + 5, posY);
+    ctx.arc(posX, posY, 5, 0, Math.PI);
+    ctx.closePath();
+    ctx.fillStyle = 'white';
+    ctx.fill();
 }
 
+const Canvas = () => {
+    const canvasRef = useRef(null);
+    const divRef = useRef(null);
+
+
+    return ( 
+        <div ref={divRef}>
+                <canvas
+                    ref={canvasRef}
+                    width={1000}
+                    height={500}
+                    onClick={e => {
+                        const canvas = canvasRef.current
+                        const ctx = canvas.getContext('2d')
+                        draw(ctx, e.clientX, e.clientY)
+                    }}>
+                </canvas>
+        </div>
+     );
+}
  
 export default Canvas;
