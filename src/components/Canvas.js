8b84a6e65148da86c3276dde80808596d895f35c
roosap <roosa.paivansalo@gmail.com>
2020-05-15 16:34:00 +0100

    Refactor

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,5 +1,5 @@
-import React, {useRef, useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import React, {useRef, useEffect} from 'react';
+
 
 const Canvas = () => {
     const canvasRef = useRef(null);
@@ -127,12 +127,9 @@ const Canvas = () => {
         }
     }, [canvasRef]);
 
+    
     return ( 
         <div>
-            <h4>Nice to meet you!</h4>
-            <h6>If you're here to read more about me, head to my <span className="white"><Link to='/about'>About Me</Link></span> section.<br /><br />
-            If you're here to have a sneak peek at my recent projects, please see <span className="white"><Link to='/projects'>My Projects</Link></span>.<br /><br />
-            Or, if you're here because you're bored, why don't you draw something below:</h6>
                 <canvas
                     ref={canvasRef}
                     width={window.innerWidth - 200}
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Canvas from '../components/Canvas';
+import {Link} from 'react-router-dom';
 
 const HomeContainer = () => {
 
 
     return (  
         <div>
+            <h4>
+                Nice to meet you!
+            </h4>
+            <h6>
+                If you're here to read more about me, head to my <span className="white"><Link to='/about'>About Me</Link></span> section.<br /><br />
+                If you're here to have a sneak peek at my recent projects, please see <span className="white"><Link to='/projects'>My Projects</Link></span>.<br /><br />
+                Or, if you're here because you're bored, why don't you draw something below:
+            </h6>
+
             <Canvas />
         </div>
     );
