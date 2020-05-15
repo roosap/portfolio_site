cf95b699084bc9d94ce27085e4016a7af76f6178
roosap <roosa.paivansalo@gmail.com>
2020-05-15 18:16:21 +0100

    Refactor Project Container

--- src/containers/ProjectContainer.js
+++ src/containers/ProjectContainer.js
@@ -3,21 +3,15 @@ import Project1 from '../components/Project_1';
 import Project2 from '../components/Project_2';
 import Project3 from '../components/Project_3';
 
-class ProjectContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div>
-                <h4>Projects</h4>
+const ProjectContainer = () => {
+    return ( 
+        <div>
+            <h4>Projects</h4>
                 <Project3 />
                 <Project2 />
                 <Project1 />
-            </div>
-         );
-    }
+         </div>
+     );
 }
  
 export default ProjectContainer;
